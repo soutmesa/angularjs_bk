b15f2b9a7ef348466ad51984ea3c90cecf931829
Mesa <mesa@MacBookProMAMA.local>
2016-04-09 20:47:44 +0700

    add insertcontroll

--- js/app.js
+++ js/app.js
@@ -2,22 +2,20 @@ angular.module('myangular', ['ngRoute'])
 .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
 	$routeProvider
 	.when('/add-new', {
-		templateUrl: 'views/add-new.html'
+		templateUrl: 'views/add-new.html',
+		controller: 'insertController'
 	})
 	.when('/edit/:ind', {
 		templateUrl: 'views/edit.html',
 		controller: 'editController'
 	})
 	.otherwise('/');
-	// $locationProvider.html5Mode(true);
 }])
 .controller('editController',['$scope', '$http', '$routeParams', '$filter', function($scope, $http, $routeParams, $filter){
 	$scope.id = $routeParams.ind;
-
 	$scope.update = update;
-
+	$scope.newPerson = {};
 	edit();
-
 	function edit() {
 		$http.get('/angularjs_bk/api.php?act=get&id=' + $scope.id)
 			.success(function(result) {
@@ -27,18 +25,34 @@ angular.module('myangular', ['ngRoute'])
 				console.log(data);
 			})
 	}
-
 	function update() {
 		$http({
 			method: 'put',
 			url: '/angularjs_bk/api.php?act=put',
 			data: $scope.newPerson
 		}).success(function(data){
-			console.log(data);
+			//console.log(data);
 			person = $filter('filter')($scope.people, {id: data.id})[0];
 		    index = $scope.people.indexOf(person) ;
 		    $scope.people[index]= data;
 			swal("Updated", "Record has been update!", "success");
+			$scope.newPerson = {};
+		});
+	}
+}])
+.controller('insertController', ['$scope', '$http', function($scope, $http){
+	$scope.insert = insert;
+	function insert() {
+		$http({
+			method: 'post',
+			url: '/angularjs_bk/api.php?act=post',
+			data:  $scope.newPerson
+		}).success(function(resopne){
+			console.log(resopne);
+			$scope.people.push(resopne);
+			swal("Inserted", "Record has been inserted!", "success");
+			//$location.url('/');
+			console.log($scope.people);
 			$scope.newPerson = {};
 		});
 	}
@@ -56,34 +70,6 @@ angular.module('myangular', ['ngRoute'])
 			})
 	}
 	$scope.show();
-	function insert() {
-
-		$http({
-			method: 'post',
-			url: '/angularjs_bk/api.php?act=post',
-			data:  $scope.newPerson
-		}).success(function(resopne){
-			console.log(resopne);
-
-			$scope.people.push(resopne);
-			
-			swal("Inserted", "Record has been inserted!", "success");
-			$location.url('/');
-			console.log($scope.people);
-			$scope.newPerson = {};
-		});
-	}
-	$scope.check = function(){
-		if(!!$scope.newPerson.id){
-			update();
-		}else{
-			insert();
-		}
-	}
-	$scope.edit = function(person){
-		$scope.newPerson = person;
-	}
-	
 	$scope.delete = function(id, index){
 		swal({
 			title: "Are you sure?",
